db68bc7bc71b253d2952368f219913e5b3b3dbac
afifrohul <afifmemyself22@gmail.com>
2023-11-03 21:31:37 +0700

    add new project

--- components/Header.vue
+++ components/Header.vue
@@ -10,7 +10,7 @@
         <a href="https://www.google.co.id/maps/place/Sekretariat+Himpunan+Mahasiswa+Informatika+(HMIF)/@-8.1657366,113.7148012,17z/data=!3m1!4b1!4m6!3m5!1s0x2dd6950a7ea1f65b:0xb5019ef2f61574b0!8m2!3d-8.1657366!4d113.7169899!16s%2Fg%2F11tfh4r0rl" target="_blank" class="text-xs lg:text-base"> Jember University | Departement of Computer Science | Informatics ↗
         </a>
         </h4>
-        <h4 class="text-gray-600 text-xs lg:text-base">Part of <span class="text-xm lg:text-base bg-gray-100 p-px lg:px-1 lg:py-1 rounded-sm lg:rounded-md border-gray-200 border-1 hover:text-gray-900 font-mono hover:bg-gray-200 duration-100 transition-all"><a href="https://www.instagram.com/hmifunej/" target="_blank">@hmifunej</a></span> as Head Sub Division of Technology</h4>
+        <h4 class="text-gray-600 text-xs lg:text-base">Part of <span class="text-xm lg:text-base bg-gray-100 p-px lg:px-1 lg:py-1 rounded-sm lg:rounded-md border-gray-200 border-1 hover:text-gray-900 font-mono hover:bg-gray-200 duration-100 transition-all"><a href="https://www.instagram.com/hmifunej/" target="_blank">@hmifunej</a></span> as Head of Technology Sub Division</h4>
       </div>
     </div>
     <hr class="mt-8">
